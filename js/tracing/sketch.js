19a337950262b7bd7b7676626b15e53d42fb61a6
karismagarduno <93623086+karismagarduno@users.noreply.github.com>
2023-11-07 16:02:52 -0700

    Update sketch.js

--- js/tracing/sketch.js
+++ js/tracing/sketch.js
@@ -43,7 +43,7 @@ function drawDashedCircle(x, y, radius, dashLength) {
 }
 
 function draw() {
-  background(206, 222, 189);
+  clear();
 
   // Draw dashed circle
   drawDashedCircle(350, 350, circleRadius, 10);
