6efae529dce20fe09d004f4eb130d8840ecbebcc
karismagarduno <93623086+karismagarduno@users.noreply.github.com>
2023-11-09 16:08:57 -0700

    Update sketch.js

--- js/tracing/sketch.js
+++ js/tracing/sketch.js
@@ -39,7 +39,7 @@ function setup() {
   
   let nextButton = createButton('Next');
   nextButton.size(200, 50);
-  nextButton.position(250,330);
+  nextButton.position(750,330);
   nextButton.style("font-size", "25px");
   nextButton.style('background-color', '#CEDEBD');
   nextButton.mouseOver(onHover3);
