3910a3c350679a5b3bf50b53e863f43ccb0a5196
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-11-21 16:11:30 -0700

    Update sketch.js

--- js/tracing1/sketch.js
+++ js/tracing1/sketch.js
@@ -229,7 +229,7 @@ function draw() {
      stroke('rgba(10,120,70, 0.5)');
      if (completedTracing) {
         textSize(22);
-        text('Tracing Completed!', 640, 160); // Display outside
+        text('Tracing Completed!', 620, 110); // Display outside
         accumulateTime = false;
         resetTracing();
       }
