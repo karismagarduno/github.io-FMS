373a74ea935419440dd4ec1c26cf128e659ec96b
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-11-29 19:12:09 -0700

    Update sketch.js

--- js/tracing1/sketch.js
+++ js/tracing1/sketch.js
@@ -9,6 +9,11 @@ let start = null; //for timer
 let o; // for timer
 let accumulateTime = true;
 let isMouseOnLine = false;
+let completedTracing = false;
+let startTime;
+let elapsedTime = 0; // Variable to store the elapsed time
+let score = 0;
+let playerScore = 0;
 
 const targetCoordinates = [
   { x: 550, y: 310 },
@@ -65,6 +70,11 @@ function setup() {
     stroke('rgba(10,120,70, 0.5)');
     textSize(26);
     text('Great job!', 643, 130);
+    if (score > elapsedTime || score == 0) {
+          score = elapsedTime;
+        }
+    elapsedTime = millis() - startTime;
+    isTracing = false; // Stop tracing
   });
   
   let startHere = createButton('^ Start Here... Move This Way >'); // starting point for the player (doesn't activate anything)
@@ -118,7 +128,7 @@ function setup() {
     isDrawing = false;
     path = [];
     startStop.html(resetText);
-    resetTracing();
+    //resetTracing();
     endShape();
   });
   
@@ -159,32 +169,42 @@ function onOut4() {
   // Change the button's color when the mouse leaves
   finish.style('background-color', '#FFFFFF');
 }
-function goToAnotherPage() {
-    window.location.href = 'tracing.html'; // Replace with the URL of the target page
-}
+  
 }  
   
 function draw() {
   
   // code that programs the timer
-  if (accumulateTime) 
-{  
-  const totalMillis = accum + (start != null ? Date.now() - start : 0);
-  const s = Math.floor(totalMillis / 1000) % 60;
-  const m = Math.floor(totalMillis / 1000 / 60) % 60;
-  const string = `Time: ${nf(m, 2)}:${nf(s, 2)}`;
-  o.html(string);
-  o.style('font-size', '20px')
-}
+  
+//   if (accumulateTime) 
+// {  
+//   const totalMillis = accum + (start != null ? Date.now() - start : 0);
+//   const s = Math.floor(totalMillis / 1000) % 60;
+//   const m = Math.floor(totalMillis / 1000 / 60) % 60;
+//   const string = `Time: ${nf(m, 2)}:${nf(s, 2)}`;
+//   o.html(string);
+//   o.style('font-size', '20px')
+// }
 
   // check to see if the user is hovering over the lines drawn
-  if(((mouseX>=545 && mouseX<=900) && (mouseY>=455 && mouseY<=465)) || ((mouseX>=545 && mouseX<=555) && (mouseY>=305 && mouseY<=465)) || ((mouseX>=850 && mouseX<=860) && (mouseY>=305 && mouseY<=465)) || ((mouseX>=620 && mouseX<=791) && (mouseY>=155 && mouseY<=165)) || ((mouseX>=550 && mouseX<=557.5) && (mouseY<=310 && mouseY>=295)) || ((mouseX>=557.5 && mouseX<=565) && (mouseY<=295 && mouseY>=280)) || ((mouseX>=565 && mouseX<=572.5) && (mouseY<=280 && mouseY>=265)) || ((mouseX>=572.5 && mouseX<=580) && (mouseY<=265 && mouseY>=250)) || ((mouseX>=580 && mouseX<=587.5) && (mouseY<=250 && mouseY>=235)) || ((mouseX>=587.5 && mouseX<=595) && (mouseY<=235 && mouseY>=220)) ||
-((mouseX>=595 && mouseX<=602.5) && (mouseY>=220 && mouseY<=205)) || ((mouseX>=602.5 && mouseX<=610) && (mouseY>=205 && mouseY<=190)) || ((mouseX>=610 && mouseX<=617.5) && (mouseY>=190 && mouseY<=175))|| ((mouseX>=617.5 && mouseX<=625) && (mouseY>=175 && mouseY<=160))|| ((mouseX>=792.9 && mouseX<=799.8) && (mouseY>=175 && mouseY<=190))|| ((mouseX>=799.8 && mouseX<=806.7) && (mouseY>=190 && mouseY<=205))|| ((mouseX>=806.7 && mouseX<=813.6) && (mouseY>=205 && mouseY<=220))|| ((mouseX>=813.6 && mouseX<=820.5) && (mouseY>=220 && mouseY<=235))|| ((mouseX>=820.5 && mouseX<=827.4) && (mouseY>=235 && mouseY<=250))|| ((mouseX>=827.4 && mouseX<=834.3) && (mouseY>=250 && mouseY<=265))|| ((mouseX>=834.3 && mouseX<=841.2) && (mouseY>=265 && mouseY<=280))|| ((mouseX>=841.2 && mouseX<=848.1) && (mouseY>=280 && mouseY<=295))|| ((mouseX>=848.1 && mouseX<=855) && (mouseY>=295 && mouseY<=310)))
+  if(((mouseX>=545 && mouseX<=900) && (mouseY>=455 && mouseY<=465)) || ((mouseX>=545 && mouseX<=555) && (mouseY>=305 && mouseY<=465)) || ((mouseX>=850 && mouseX<=860) && (mouseY>=305 && mouseY<=465)) || ((mouseX>=620 && mouseX<=791) && (mouseY>=155 && mouseY<=165)) || ((mouseX>=550 && mouseX<=557.5) && (mouseY<=310 && mouseY>=295)) || ((mouseX>=557.5 && mouseX<=565) && (mouseY<=295 && mouseY>=280)) || ((mouseX>=565 && mouseX<=572.5) && (mouseY<=280 && mouseY>=265)) || ((mouseX>=572.5 && mouseX<=580) && (mouseY<=265 && mouseY>=250)) ||((mouseX>=587.5 && mouseX<=595) && (mouseY<=235 && mouseY>=220)) ||
+((mouseX>=595 && mouseX<=602.5) && (mouseY>=220 && mouseY<=205)) ||
+((mouseX>=602.5 && mouseX<=610) && (mouseY>=205 && mouseY<=190)) ||
+((mouseX>=610 && mouseX<=617.5) && (mouseY>=190 && mouseY<=175))||
+((mouseX>=617.5 && mouseX<=625) && (mouseY>=175 && mouseY<=160))||
+((mouseX>=786 && mouseX<=792.9) && (mouseY>=160 && mouseY<=175))||
+((mouseX>=792.9 && mouseX<=799.8) && (mouseY>=175 && mouseY<=190))|| ((mouseX>=799.8 && mouseX<=806.7) && (mouseY>=190 && mouseY<=205))|| ((mouseX>=806.7 && mouseX<=813.6) && (mouseY>=205 && mouseY<=220))|| ((mouseX>=813.6 && mouseX<=820.5) && (mouseY>=220 && mouseY<=235))|| ((mouseX>=820.5 && mouseX<=827.4) && (mouseY>=235 && mouseY<=250))|| ((mouseX>=827.4 && mouseX<=834.3) && (mouseY>=250 && mouseY<=265))|| ((mouseX>=834.3 && mouseX<=841.2) && (mouseY>=265 && mouseY<=280))|| ((mouseX>=841.2 && mouseX<=848.1) && (mouseY>=280 && mouseY<=295))|| ((mouseX>=848.1 && mouseX<=855) && (mouseY>=295 && mouseY<=310)))
     {
       isMouseOnLine = true;
+      if (!startTime) {
+        startTime = millis();
+      }
     }
   else{
     isMouseOnLine = false;
+    if (startTime) {
+        elapsedTime = millis() - startTime;
+      }
   }
   
   stroke(0);
@@ -223,9 +243,9 @@ function draw() {
   const bad = 'Don\'t give up';
   const good = "Keep it up!!!!!";
   enc = createButton(bad);
-  enc.position(640,420);
-  enc.style('background-color', '#CEDEBD');
-  enc.style('border', 'none');
+  enc.position(625,310);
+  enc.style('background-color', '#9EB384')
+  enc.style('stroke', '3');
   enc.style('font-family', 'Expo');
   enc.style('font-size', '26px')
   
@@ -238,32 +258,47 @@ function draw() {
       enc.html(bad);
       isDrawing = false;
     }
-
-  let completedTracing = false;
   
   // creates a moving line behind the user's mouse if they are hovering over the perimeter
     if (isDrawing) 
     {
      stroke('rgba(10,120,70, 0.5)');
-      path.push(createVector(mouseX, mouseY));
-      line(pmouseX, pmouseY, mouseX, mouseY);
-
+     draws = path.push(createVector(mouseX, mouseY));
+     line(pmouseX, pmouseY, mouseX, mouseY);
+      
       completedTracing = true;
-      for (let targetCoord of targetCoordinates) {
+      for (let targetCoord of targetCoordinates){
         if (!hasTracedCoordinate(targetCoord)) {
           completedTracing = false;
           break;
+         // } else completedTracing = true;
         }
+        
+//         if (completedTracing) {
+//         if (score > elapsedTime || score == 0) {
+//           score = elapsedTime;
+//         }
+
+//         textSize(25);
+//         fill(67, 83, 52);
+//         strokeWeight(5);
+//         textAlign(CENTER, CENTER);
+//         //text('Tracing Completed!', 700, 400);
+//         // Stop the timer when tracing is complete
+//         elapsedTime = millis() - startTime;
+//         isTracing = false; // Stop tracing
+//       }
       }
-    }
   
      stroke('rgba(10,120,70, 0.5)');
+  
      if (completedTracing) {
         textSize(26);
         text('Tracing Completed!', 610, 120); // Display outside
         accumulateTime = false;
         resetTracing();
-      }
+       endShape();
+     }
   
       strokeWeight(3);
       beginShape();
@@ -273,23 +308,66 @@ function draw() {
           vertex(point.x, point.y);
         }
       endShape();
+    }
+  if (score > 0) {
+      // textSize(25);
+      // fill(67, 83, 52);
+      // strokeWeight(5);
+      // textAlign(CENTER, CENTER);
+      // text('Tracing Completed!', 700, 400);
+      //tracing completed should be gone once the reset button is clicked
+      playerScore = score;
+  }
+  
+  fill(255);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text(`Time: ${time(elapsedTime)}`, 550, 550);
+  text(`Score:${time(playerScore)}`, 830, 550);
+
+}
+
+function time(mili){
+if (accumulateTime) 
+{  
+  let seconds = Math.floor(mili / 1000);
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  return nf(minutes, 2) + ':' + nf(seconds, 2);
+  // const totalMillis = accum + (start != null ? Date.now() - start : 0);
+  // const s = Math.floor(totalMillis / 1000) % 60;
+  // const m = Math.floor(totalMillis / 1000 / 60) % 60;
+  // const string = `Time: ${nf(m, 2)}:${nf(s, 2)}`;
+  // o.html(string);
+  // o.style('font-size', '20px')
+}
+}
+
+function formatTime(milliseconds) {
+   let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  return nf(minutes, 2) + ':' + nf(seconds, 2);
 }
 
 function startTracing() {
-//  isTracing = true;
-  isDrawing = true;
+ // isTracing = true;
   path = [];
+  isDrawing = false;
   tracedCoordinates = [];
+  startTime = millis(); 
 }
 
 function resetTracing() {
+  // isDrawing = false;
+  // tracedCoordinates = [];
   //isTracing = false;
-  textSize(18);
-  text(' ', 50, 300);
   isDrawing = false;
   tracedCoordinates = [];
+  startTime = null; // Reset the start time when the user resets tracing
   accumulateTime = false;
-  start = null;
+  path = []; 
+  elapsedTime = 0;
 }
 
 function hasTracedCoordinate(targetCoord) {
